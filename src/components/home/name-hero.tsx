bdba2c129694fdcf88d44448c3c469497d24c731
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2020-12-06 00:20:18 -0500

    Added hanzi svgs and index page hero component

--- src/components/home/name-hero.tsx
+++ src/components/home/name-hero.tsx
@@ -1,9 +1,71 @@
-import {motion} from 'framer-motion';
+/** @jsxImportSource @emotion/react */
+import {motion, Variants} from 'framer-motion';
+import {Flex} from 'src/components/base';
+
+// const parentVariants: Variants = {
+//   visible: {
+//     transition: {
+//       delay: 1,
+//       duration: 2,
+//       staggerChildren: 1,
+//     },
+//   },
+// };
+
+const firstNameVariant1: Variants = {
+  hidden: {
+    // opacity: 0,
+    y: 100,
+  },
+  visible: {
+    // opacity: 1,
+    y: 0,
+    transition: {
+      delay: 1,
+      duration: 1,
+      staggerChildren: 1,
+    },
+  },
+};
+
+const firstNameVariant2: Variants = {
+  hidden: {
+    // opacity: 0,
+    y: -100,
+  },
+  visible: {
+    // opacity: 1,
+    y: 0,
+    transition: {
+      delay: 1,
+      duration: 1,
+      staggerChildren: 1,
+    },
+  },
+};
+
+const fontSize = '18rem';
 
 export const Hero = (): JSX.Element => {
   return (
-    <motion.div>
-      <div>wow</div>
-    </motion.div>
+    <Flex flow='column' css={{userSelect: 'none'}}>
+      <Flex css={{overflow: 'hidden', fontSize, height: fontSize, lineHeight: fontSize}}>
+        <motion.div initial='hidden' animate='visible' variants={firstNameVariant1}>
+          DAN
+        </motion.div>
+        <motion.div initial='hidden' animate='visible' variants={firstNameVariant2}>
+          IEL
+        </motion.div>
+      </Flex>
+      <Flex css={{overflow: 'hidden', fontSize, height: fontSize, lineHeight: fontSize}}>
+        <motion.div initial='hidden' animate='visible' variants={firstNameVariant2}>
+          ZHA
+        </motion.div>
+        <motion.div initial='hidden' animate='visible' variants={firstNameVariant1}>
+          NG
+        </motion.div>
+      </Flex>
+      <Flex>software engineer // entrepreneur</Flex>
+    </Flex>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 /** @jsxImportSource @emotion/react */
-import {Flex} from 'src/components/base';
-import {MainLayout} from 'src/components/layouts';
+import {Flex, Grid, GridItem} from 'src/components/base';
+import {Hero} from 'src/components/home';
+import {Hanzi} from 'src/components/home/hanzi';
 
 export default function HomePage(): JSX.Element {
   return (
-    <Flex>
-      <div>DANIEL</div>
-      <div>ZHANG</div>
-    </Flex>
+    <Grid templateColumns='auto 30%' css={{height: '100vh'}}>
+      <GridItem justify='end' align='center'>
+        <Hero />
+      </GridItem>
+      <GridItem>
+        <Flex>
+          <Hanzi type='zhang' />
+        </Flex>
+      </GridItem>
+      <div
+        css={{
+          position: 'fixed',
+          bottom: '4rem',
+          left: '4rem',
+          transformOrigin: 'top left',
+          transform: 'rotate(-90deg)',
+        }}
+      >
+        ---- SCROLL TO SEE MORE
+      </div>
+    </Grid>
   );
 }
