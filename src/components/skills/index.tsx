0f9e3a90da20abe97086bcd86dff1353f65f8b4e
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2021-10-19 23:57:21 -0400

    Support wrapping columns for small screens

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,22 +1,25 @@
-import {Flex, SimpleGrid, useBreakpoint} from '@chakra-ui/react';
+import {Flex, Grid} from '@chakra-ui/react';
 import {SectionTitle} from 'src/components/common';
 import {SkillsRow} from 'src/components/skills/row';
 import {skills} from 'src/content';
 
 export const Skills = (): JSX.Element => {
-  const breakpoint = useBreakpoint();
-  // const breakpoint = useBreakpointValue({lg: '3'});
-  console.log(breakpoint);
   return (
     <Flex direction='column'>
       <SectionTitle heading='Skills' subheading='TECHNOLOGIES I HAVE USED EXTENSIVELY' />
-      <SimpleGrid columns={5} spacingY='3.6rem' minChildWidth='15rem'>
+      <Grid
+        templateColumns={{
+          base: 'repeat(auto-fit, minmax(15rem, 1fr))', // auto-fit to wrap columns on small screens
+          xl: 'repeat(5, 15rem)', // use justify space-between for large screens
+        }}
+        justifyContent='space-between'
+      >
         <SkillsRow title='Languages' data={skills.languages} />
         <SkillsRow title='Frameworks' data={skills.frameworks} />
         <SkillsRow title='Runtimes' data={skills.runtimes} />
         <SkillsRow title='Databases' data={skills.databases} />
         <SkillsRow title='Tools' data={skills.tools} />
-      </SimpleGrid>
+      </Grid>
     </Flex>
   );
 };
