51ff5375b71bf4f525e5b192a8a5041d852e055b
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2021-11-02 23:00:18 -0400

    Check if new visitor to determine whether animation is skippable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
-import {Flex, Stack, useBreakpoint, useTheme} from '@chakra-ui/react';
+import {Flex, Stack, useTheme} from '@chakra-ui/react';
 import {useState} from 'react';
 import {Hero} from 'src/components/home';
 import {Projects} from 'src/components/projects';
 import {Skills} from 'src/components/skills';
+import {IS_BROWSER} from 'src/config/constants';
 
 export default function HomePage(): JSX.Element {
-  const breakpoint = useBreakpoint();
-  console.log(breakpoint);
   const theme = useTheme();
-  const [finished, setFinished] = useState(true); // TODO: change back to false
+  const [isContentShowing, setIsContentShowing] = useState(() => {
+    if (IS_BROWSER && !localStorage.getItem('new-visitor')) {
+      localStorage.setItem('new-visitor', 'true');
+      return false;
+    }
+    return true;
+  });
+
   return (
     <Flex direction='column' bgColor='black' minW={theme.__breakpoints?.asObject.sm}>
       <Flex
@@ -17,11 +23,11 @@ export default function HomePage(): JSX.Element {
         align='flex-start'
         grow={1}
         justify={['flex-start', null, null, 'center']}
-        pt={['14vh', null, null, '20vh']}
+        pt={['12vh', null, null, '20vh']}
       >
-        <Hero onAnimationEnd={() => setFinished(true)} />
+        <Hero onAnimationEnd={() => setIsContentShowing(true)} />
       </Flex>
-      {finished && (
+      {isContentShowing && (
         <Stack
           direction='column'
           spacing='8rem'
