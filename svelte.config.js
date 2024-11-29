e7be912289833106ad8665d0b10e41cfd8c61f74
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2024-11-29 12:52:42 -0500

    chore: properly ignore compiler warnings

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
+	},
+	compilerOptions: {
+		// disable all warnings coming from node_modules and all accessibility warnings
+		warningFilter: (warning) => !warning.filename?.includes('node_modules') && !warning.code.startsWith('a11y'),
 	},
 };
 
